3ec46877c4b5b49e3caa7d828173190243dcafe2
ssribeiro <luan@ssribeiro.com>
2019-07-30 01:17:40 -0300

    error dealer package added

---
+++ src/error.spec.ts
@@ -0,0 +1,280 @@
+/* tslint:disable:no-unused-expression */
+import { expect } from 'chai'
+import { spy } from 'sinon'
+
+import chalk from 'chalk'
+
+import {
+    error,
+    JobtestError,
+    OPERATIONAL_PREFIX,
+    DEFAULT_ERROR_MESSAGE,
+} from './error'
+
+describe('error', () => {
+    it('should have an expected JobtestError class', () => {
+        expect(JobtestError).to.be.exist
+        let jobtestError = new JobtestError()
+        expect(jobtestError).to.be.exist
+        expect(jobtestError instanceof Error).to.be.true
+        expect(jobtestError.message).to.be.equal(DEFAULT_ERROR_MESSAGE)
+        expect(jobtestError.details).to.be.an('array').that.is.empty
+
+        jobtestError = new JobtestError('some message')
+        expect(jobtestError instanceof Error).to.be.true
+        expect(jobtestError.message).to.be.equal('some message')
+        expect(jobtestError.details).to.be.an('array').that.is.empty
+
+        jobtestError = new JobtestError('some message', 'some other detail', {
+            detail: 'object',
+        })
+        expect(jobtestError instanceof Error).to.be.true
+        expect(jobtestError.message).to.be.equal('some message')
+        expect(jobtestError.details[0]).to.be.equal('some other detail')
+        expect(jobtestError.details[1]).to.be.deep.equal({ detail: 'object' })
+    })
+
+    describe('is function', () => {
+        it('act as shortcut for creating errors', () => {
+            expect(error.is).to.be.exist
+            let jobtestError = error.is()
+            expect(jobtestError).to.be.exist
+            expect(jobtestError instanceof Error).to.be.true
+            expect(jobtestError.message).to.be.equal(DEFAULT_ERROR_MESSAGE)
+            expect(jobtestError.details).to.be.an('array').that.is.empty
+
+            jobtestError = error.is('some message')
+            expect(jobtestError instanceof Error).to.be.true
+            expect(jobtestError.message).to.be.equal('some message')
+            expect(jobtestError.details).to.be.an('array').that.is.empty
+
+            jobtestError = error.is('some message', 'some other detail', {
+                detail: 'object',
+            })
+            expect(jobtestError instanceof Error).to.be.true
+            expect(jobtestError.message).to.be.equal('some message')
+            expect(jobtestError.details[0]).to.be.equal('some other detail')
+            expect(jobtestError.details[1]).to.be.deep.equal({
+                detail: 'object',
+            })
+        })
+    })
+
+    describe('throw function', () => {
+        it('throw errors in expected ways', () => {
+            expect(error.throw).to.be.exist
+
+            try {
+                error.throw()
+            } catch (jobtestError) {
+                expect(jobtestError).to.be.exist
+                expect(jobtestError instanceof JobtestError).to.be.true
+                expect(jobtestError.message).to.be.equal(DEFAULT_ERROR_MESSAGE)
+                expect(jobtestError.details).to.be.an('array').that.is.empty
+            }
+
+            try {
+                error.throw('some message')
+            } catch (jobtestError) {
+                expect(jobtestError).to.be.exist
+                expect(jobtestError instanceof JobtestError).to.be.true
+                expect(jobtestError.message).to.be.equal('some message')
+                expect(jobtestError.details).to.be.an('array').that.is.empty
+            }
+
+            try {
+                error.throw('some message', 'some other detail', {
+                    detail: 'object',
+                })
+            } catch (jobtestError) {
+                expect(jobtestError).to.be.exist
+                expect(jobtestError instanceof JobtestError).to.be.true
+                expect(jobtestError.message).to.be.equal('some message')
+                expect(jobtestError.details[0]).to.be.equal('some other detail')
+                expect(jobtestError.details[1]).to.be.deep.equal({
+                    detail: 'object',
+                })
+            }
+
+            try {
+                error.throw(new JobtestError('jobtest error'))
+            } catch (jobtestError) {
+                expect(jobtestError).to.be.exist
+                expect(jobtestError instanceof JobtestError).to.be.true
+                expect(jobtestError.message).to.be.equal('jobtest error')
+                expect(jobtestError.details).to.be.an('array').that.is.empty
+            }
+
+            try {
+                error.throw(new Error('strange error'))
+            } catch (jobtestError) {
+                expect(jobtestError).to.be.exist
+                expect(jobtestError.message).to.be.equal('strange error')
+            }
+        })
+        
+        it('throw JobtestErrors in expected ways', () => {
+            expect(error.throwJobtestError).to.be.exist
+
+            try {
+                error.throwJobtestError()
+            } catch (jobtestError) {
+                expect(jobtestError).to.be.exist
+                expect(jobtestError instanceof JobtestError).to.be.true
+                expect(jobtestError.message).to.be.equal(DEFAULT_ERROR_MESSAGE)
+                expect(jobtestError.details).to.be.an('array').that.is.empty
+            }
+
+            try {
+                error.throwJobtestError('some message')
+            } catch (jobtestError) {
+                expect(jobtestError).to.be.exist
+                expect(jobtestError instanceof JobtestError).to.be.true
+                expect(jobtestError.message).to.be.equal('some message')
+                expect(jobtestError.details).to.be.an('array').that.is.empty
+            }
+
+            try {
+                error.throwJobtestError('some message', 'some other detail', {
+                    detail: 'object',
+                })
+            } catch (jobtestError) {
+                expect(jobtestError).to.be.exist
+                expect(jobtestError instanceof JobtestError).to.be.true
+                expect(jobtestError.message).to.be.equal('some message')
+                expect(jobtestError.details[0]).to.be.equal('some other detail')
+                expect(jobtestError.details[1]).to.be.deep.equal({
+                    detail: 'object',
+                })
+            }
+
+            try {
+                error.throwJobtestError(new JobtestError('jobtest error'))
+            } catch (jobtestError) {
+                expect(jobtestError).to.be.exist
+                expect(jobtestError instanceof JobtestError).to.be.true
+                expect(jobtestError.message).to.be.equal('Error: jobtest error')
+                expect(jobtestError.details).to.be.an('array').that.is.empty
+            }
+
+            try {
+                error.throwJobtestError(new Error('strange error'))
+            } catch (jobtestError) {
+                expect(jobtestError).to.be.exist
+                expect(jobtestError.message).to.be.equal('Error: strange error')
+            }
+        })
+        
+    })
+
+    describe('op function', () => {
+        it('should exists', () => {
+            expect(error.op).to.be.exist
+        })
+
+        it('should warn in logger a void error', () => {
+            const consoleLogSpy = spy(console, 'log')
+            error.op()
+            consoleLogSpy.should.have.been.calledWith(
+                chalk.yellow(OPERATIONAL_PREFIX + DEFAULT_ERROR_MESSAGE)
+            )
+            consoleLogSpy.restore()
+        })
+
+        it('should warn in logger a message error', () => {
+            const consoleLogSpy = spy(console, 'log')
+            error.op('some error')
+            consoleLogSpy.should.have.been.calledWith(
+                chalk.yellow(OPERATIONAL_PREFIX + 'some error')
+            )
+            consoleLogSpy.restore()
+        })
+
+        it('should warn in logger a detailed error', () => {
+            const consoleLogSpy = spy(console, 'log')
+            error.op('detailed error', 'with', { several: 'details' })
+            consoleLogSpy.should.have.been.calledWith(
+                chalk.yellow(OPERATIONAL_PREFIX + 'detailed error')
+            )
+            consoleLogSpy.should.have.been.calledWith(chalk.yellow('with'))
+            consoleLogSpy.restore()
+        })
+
+        it('should warn already existent void error', () => {
+            const consoleLogSpy = spy(console, 'log')
+            error.op(new Error())
+            expect(console.log).to.have.been.calledWith(
+                chalk.yellow(OPERATIONAL_PREFIX + DEFAULT_ERROR_MESSAGE)
+            )
+            consoleLogSpy.restore()
+        })
+
+        it('should warn already existent message error', () => {
+            const consoleLogSpy = spy(console, 'log')
+            error.op(new Error('some error'))
+            expect(console.log).to.have.been.calledWith(
+                chalk.yellow(OPERATIONAL_PREFIX + 'some error')
+            )
+            consoleLogSpy.restore()
+        })
+
+        it('should warn already existent jobtest error', () => {
+            const consoleLogSpy = spy(console, 'log')
+            error.op(error.is('jobtest error'))
+            expect(console.log).to.have.been.calledWith(
+                chalk.yellow(OPERATIONAL_PREFIX + 'jobtest error')
+            )
+            consoleLogSpy.restore()
+        })
+
+        it('should warn already existent jobtest detailed error', () => {
+            const consoleLogSpy = spy(console, 'log')
+            error.op(error.is('detailed error', 'with', { several: 'details' }))
+            expect(console.log).to.have.been.calledWith(
+                chalk.yellow(OPERATIONAL_PREFIX + 'detailed error')
+            )
+            consoleLogSpy.restore()
+        })
+    })
+})
+
+/*
+describe('logger', () => {
+    it('should have the error, warn, success and log functions', () => {
+        expect(logger.error).to.be.exist
+        expect(logger.warn).to.be.exist
+        expect(logger.success).to.be.exist
+        expect(logger.log).to.be.exist
+    })
+
+    it('should do error, warn, success and log', () => {
+        logger.error('error will be like this')
+        logger.warn('warn will be like this')
+        logger.success('success will be like this')
+        logger.log('log will be like this')
+    })
+
+    it('should ignore dev marks when not developing', () => {
+        process.env.JOBTEST_ENV = 'otherThanDev'
+        const consoleLogSpy = spy(console, 'log')
+        const stringTest = 'string test'
+        logger.dev(stringTest)
+        consoleLogSpy.should.have.been.not.called
+        consoleLogSpy.restore()
+    })
+
+    it('should ignore dev marks when not developing', () => {
+        process.env.JOBTEST_ENV = 'dev'
+        const consoleLogSpy = spy(console, 'log')
+        const stringTest = 'string test'
+        logger.dev(stringTest)
+        consoleLogSpy.should.have.been.calledWith(stringTest)
+        consoleLogSpy.restore()
+    })
+})
+
+
+/*
+
+
+*/
---
+++ src/error.ts
@@ -0,0 +1,121 @@
+import { logger } from './logger'
+
+export const JOBTEST_ERROR: string = 'JOBTEST ERROR'
+export const OPERATIONAL_PREFIX: string = 'OPERATIONAL ERROR: '
+export const FATAL_PREFIX: string = 'FATAL ERROR: '
+export const DEFAULT_ERROR_MESSAGE = 'unknown error'
+
+/**
+ * Centralized unique error type
+ */
+export class JobtestError extends Error {
+    public readonly type: string = JOBTEST_ERROR
+    public readonly details: any[]
+
+    /**
+     * @param message string optional
+     * @param details any details
+     */
+    constructor(message?: string, ...details: any[]) {
+        super(message || DEFAULT_ERROR_MESSAGE)
+        this.details = details
+    }
+}
+
+/**
+ * shortcuts functions to make easy dealing with errors
+ */
+export const error = {
+    /**
+     * creates the error (same as new Error(...))
+     * @param message string optional
+     * @param details any details
+     */
+    is(message?: string, ...details: any[] | any): JobtestError {
+        if (!message) {
+            return new JobtestError()
+        } else {
+            return new JobtestError(message, ...details)
+        }
+    },
+
+    /**
+     * print error to console
+     * @param  message the text message of the error OR the generated error itself (Error or JobtestError)
+     * @param  ...details any number of objects to include with the error
+     */
+    op(message?: string | Error, ...details: any[]) {
+        if (!message) {
+            logger.warn(OPERATIONAL_PREFIX + DEFAULT_ERROR_MESSAGE)
+        } else if (typeof message === 'string') {
+            logger.warn(OPERATIONAL_PREFIX + error.is(message).message)
+        } else if (message instanceof JobtestError) {
+            logger.warn(
+                OPERATIONAL_PREFIX + (message.message || DEFAULT_ERROR_MESSAGE)
+            )
+            message.details.forEach(detail => {
+                logger.warn(detail)
+            })
+        } else {
+            logger.warn(
+                OPERATIONAL_PREFIX + (message.message || DEFAULT_ERROR_MESSAGE)
+            )
+        }
+
+        details.forEach(detail => {
+            logger.warn(detail)
+        })
+    },
+
+    /**
+     * print a fatal error to console and terminates the application
+     * @param  message the text message of the error OR the generated error itself (Error or JobtestError)
+     * @param  ...details any number of objects to include with the error
+     */
+    fatal(message?: string | Error, ...details: any[]) {
+        if (!message) {
+            logger.error(FATAL_PREFIX + DEFAULT_ERROR_MESSAGE)
+        } else if (typeof message === 'string') {
+            logger.error(FATAL_PREFIX + error.is(message).message)
+        } else if (message instanceof JobtestError) {
+            logger.error(
+                FATAL_PREFIX + (message.message || DEFAULT_ERROR_MESSAGE)
+            )
+            message.details.forEach(detail => {
+                logger.error(detail)
+            })
+        } else {
+            logger.error(
+                FATAL_PREFIX + (message.message || DEFAULT_ERROR_MESSAGE)
+            )
+        }
+
+        details.forEach(detail => {
+            logger.error(detail)
+        })
+
+        // It should terminate the application to avoid unpredicted states
+        // try it gracefully
+
+        process.exit(1)
+    },
+
+    /**
+     * throw a new error with the message
+     * @param  message can be a string or an instance of Error
+     * @param  ...details any number of objects to include with the error
+     */
+    throw(message?: string | Error, ...details: any[] | any) {
+        if (!message) {
+            throw new JobtestError()
+        } else if (typeof message === 'string') {
+            throw new JobtestError(message, ...details)
+        } else {
+            throw message
+        }
+    },
+
+    throwJobtestError(...args: any | any[]) {
+        throw error.is(...args)
+    },
+}
